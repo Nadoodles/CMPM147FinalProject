f97d5075049e7f4c70403f5fa9a988a64350a7a4
Brent Chou <brentchou@Brents-MacBook-Pro-2.local>
2024-06-09 21:50:44 -0700

    changing world key position

--- js/sketch.js
+++ js/sketch.js
@@ -170,6 +170,7 @@ function setup() {
   let label = createP();
   label.html("World Key: ");
   label.parent("canvas-container");
+  label.position(100, 100);
   input = createInput("727");
   input.parent(label);
   input.input(() => {
