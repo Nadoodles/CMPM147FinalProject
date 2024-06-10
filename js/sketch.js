a4f5cf6baeb09e755a97a5a178d4ad314b3f47ae
Brent Chou <brentchou@Brents-MacBook-Pro-2.local>
2024-06-09 22:17:21 -0700

    fixing world key back to og position

--- js/sketch.js
+++ js/sketch.js
@@ -170,7 +170,7 @@ function setup() {
   let label = createP();
   label.html("World Key: ");
   label.parent("canvas-container");
-  label.position(1100, 375);
+  label.style('color', 'white');
   input = createInput("727");
   input.parent(label);
   input.input(() => {
